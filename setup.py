c7c7e01432426bd123f6edefab4f7dbbe2187cc4
JenswBE <jens@jensw.be>
2019-09-25 21:51:45 +0200

    Bump version: 0.1.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='melthon',
-    version='0.1.0',
+    version='1.0.0',
     license='GPLv3',
     description='Minimalistic static site generator',
     long_description='%s\n%s' % (
